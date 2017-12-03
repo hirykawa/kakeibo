85b7111427d657c7c3ff1d075469059ff50e1789
Hiryu Kawaguchi <hiryu.tubasa@gmail.com>
2017-12-03 15:26:22 +0900

    pgsqlをconfigに追加

--- config/database.php
+++ config/database.php
@@ -1,28 +1,39 @@
 <?php
- return [
-     'fetch' => PDO::FETCH_CLASS,
-          'default' => env('DB_CONNECTION', 'mysql'),
-          'connections' => [
-              'mysql' => [
-              'driver'    => 'mysql',
-              'host'      => env('DB_HOST', 'localhost'),
-//              'unix_socket'   => '/var/lib/mysql/mysql.sock',
-              'database'  => env('DB_DATABASE', ''),
-              'username'  => env('DB_USERNAME', ''),
-              'password'  => env('DB_PASSWORD', ''),
-              'charset'   => 'utf8',
-              'collation' => 'utf8_unicode_ci',
-              'prefix'    => '',
-              'strict'    => false,
-          ],
-      ],
-      'migrations' => 'migrations',
-      'redis' => [
-          'cluster' => false,
-          'default' => [
-              'host'     => '127.0.0.1',
-              'port'     => 6379,
-              'database' => 0,
-          ],
-      ],
-  ];
+return [
+    'fetch' => PDO::FETCH_CLASS,
+    'default' => env('DB_CONNECTION', 'mysql'),
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+    ],
+    'migrations' => 'migrations',
+    'redis' => [
+        'cluster' => false,
+        'default' => [
+            'host' => '127.0.0.1',
+            'port' => 6379,
+            'database' => 0,
+        ],
+    ],
+];
